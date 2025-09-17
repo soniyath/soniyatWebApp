f20534754ba6126929ee7ef93ef1c42652aff375
yatharthsoni <yatharth.soni14@gmail.com>
2025-09-17 11:08:00 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "Hello from Azure App Service (Python)!"
+
+# For local testing only:
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
